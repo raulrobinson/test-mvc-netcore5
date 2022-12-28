18680102f2df43b7e0d57f1e785ba560b59e7c60
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2022-12-28 10:52:49 -0500

    Agregar archivos de proyecto.

---
+++ test-mvc-netcore5/Controllers/EmployeeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using test_mvc_netcore5.Models;
+
+namespace test_mvc_netcore5.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Repository.AllEmpoyees);
+        }
+
+        // HTTP GET VERSION
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Create(employee);
+                return View("Thanks", employee);
+            }
+            else
+                return View();
+        }
+
+        public IActionResult Update(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            return View(employee);
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Update(Employee employee, string empname)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Age = employee.Age;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Salary = employee.Salary;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Department = employee.Department;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Sex = employee.Sex;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Name = employee.Name;
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ test-mvc-netcore5/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace test_mvc_netcore5.Models
+{
+    public static class Repository
+    {
+        private static List<Employee> allEmpoyees = new List<Employee>();
+
+        public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+
+        public static void Create(Employee employee)
+        {
+            allEmpoyees.Add(employee);
+        }
+
+        public static void Delete(Employee employee)
+        {
+            allEmpoyees.Remove(employee);
+        }
+    }
+}
